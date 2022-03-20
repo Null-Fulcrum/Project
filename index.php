eac95b9864ac86d8bc44c67342a5888acf34fe53
Никита Василинин <fulcrum@MacBook-Pro-Nikita.local>
2022-03-20 12:12:39 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="ru" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="style.css">
+    <title></title>
+  </head>
+  <body>
+    <div class="main">
+      <div class="main__ellipse main__ellipse-top">
+    
+      </div>
+      <div class="main__ellipse main__ellipse-bottom">
+
+      </div>
+      <div class="content">
+        <div class="leftside">
+
+        </div>
+        <div class="rightside">
+
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
